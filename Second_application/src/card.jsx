f9496066771ff4aa670533c087f77e33b7bbf367
Mukunda Mahat <mahatmuku567@gmail.com>
2025-07-20 17:53:47 +0545

    Card Components

---
+++ Second_application/src/card.jsx
@@ -0,0 +1,11 @@
+function Card(){
+    return(
+        <div className="card">
+            <img className="cardimage" src="My.jpg" alt="Muku" width={200} height={200}/>
+            <h2 className="card-title">Mukunda Mahat</h2>
+            <p className="card-class">Hi! I am a Frontend Developer</p>
+        </div>
+    )
+
+}
+export default Card
